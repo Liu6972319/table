92f11aac739aea359d68f35594cb64a13d4eb7c7
Maximiliano Jose Fernandez Wortman <maxifwortman@gmail.com>
2018-09-14 10:04:23 -0300

    controller

--- src/main/java/com/example/table/Controller/CustomerController.java
+++ src/main/java/com/example/table/Controller/CustomerController.java
@@ -5,6 +5,8 @@ import com.example.table.repository.CustomerRespository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +20,12 @@ public class CustomerController {
   @GetMapping(value = "/all")
   public List<Customer> getCustomers() {
     return customerRespository.findAll();
+  }
+
+  @PostMapping(value = "/save")
+  public String postCustomer(@RequestBody List<Customer> customers) {
+    customerRespository.saveAll(customers);
+    return "Post Successfully!";
   }
 
 }
